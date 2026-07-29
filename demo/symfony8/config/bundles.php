042f9de42d766f6bf79b6e00ca7b66992fff7120
Héctor Franco <hectorfranco@nowo.tech>
2026-07-29 19:48:41 +0200

    style: apply php-cs-fixer to demo Symfony 8 config

--- demo/symfony8/config/bundles.php
+++ demo/symfony8/config/bundles.php
@@ -1,10 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
+use Nowo\SeoKitBundle\SeoKitBundle;
+use Nowo\TwigInspectorBundle\NowoTwigInspectorBundle;
+use Symfony\Bundle\DebugBundle\DebugBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Bundle\WebProfilerBundle\WebProfilerBundle;
+
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Nowo\SeoKitBundle\SeoKitBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
-    Nowo\TwigInspectorBundle\NowoTwigInspectorBundle::class => ['dev' => true, 'test' => true],
+    FrameworkBundle::class         => ['all' => true],
+    TwigBundle::class              => ['all' => true],
+    SeoKitBundle::class            => ['all' => true],
+    WebProfilerBundle::class       => ['dev' => true, 'test' => true],
+    DebugBundle::class             => ['dev' => true],
+    NowoTwigInspectorBundle::class => ['dev' => true, 'test' => true],
 ];
