2a92f59c3f91b21f0defe35b9ace7399320daade
Héctor Franco <hectorfranco@nowo.tech>
2026-09-03 19:56:26 +0200

    style: fix demo bundles.php CS (HotReload alignment/imports)

--- demo/symfony8/config/bundles.php
+++ demo/symfony8/config/bundles.php
@@ -2,22 +2,21 @@
 
 declare(strict_types=1);
 
-use Nowo\SeoKitBundle\SeoKitBundle;
 use Nowo\HotReloadBundle\NowoHotReloadBundle;
+use Nowo\SeoKitBundle\SeoKitBundle;
 use Nowo\TwigInspectorBundle\NowoTwigInspectorBundle;
 use Symfony\Bundle\DebugBundle\DebugBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\TwigBundle\TwigBundle;
 use Symfony\Bundle\WebProfilerBundle\WebProfilerBundle;
 use Twig\Extra\TwigExtraBundle\TwigExtraBundle;
-
 return [
     FrameworkBundle::class         => ['all' => true],
     TwigBundle::class              => ['all' => true],
     SeoKitBundle::class            => ['all' => true],
     WebProfilerBundle::class       => ['dev' => true, 'test' => true],
     DebugBundle::class             => ['dev' => true],
-    NowoHotReloadBundle::class => ['dev' => true, 'test' => true],
+    NowoHotReloadBundle::class     => ['dev' => true, 'test' => true],
     NowoTwigInspectorBundle::class => ['dev' => true, 'test' => true],
     TwigExtraBundle::class         => ['all' => true],
 ];
